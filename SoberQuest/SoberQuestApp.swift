39d3265b8cf480e5739fa503cdee42fd58453160
Ben <ben.m.hatch@gmail.com>
2026-01-01 09:47:35 -0500

    Initial Commit

---
+++ SoberQuest/SoberQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoberQuestApp.swift
+//  SoberQuest
+//
+//  Created by Moon Jou Kim on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SoberQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoberQuestTests/SoberQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoberQuestTests.swift
+//  SoberQuestTests
+//
+//  Created by Moon Jou Kim on 1/1/26.
+//
+
+import Testing
+@testable import SoberQuest
+
+struct SoberQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
